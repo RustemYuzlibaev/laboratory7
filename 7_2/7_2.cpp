89e43bec5a596123b71b877f4855fbe4d2136b27
Камалов Камиль <kamalov-kamil@outlook.com>
2020-03-13 21:56:33 +0600

    Добавлены коды BASIC и результаты для удобства. Исправлена ЛР2

--- 7_2/7_2.cpp
+++ 7_2/7_2.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <cmath>
+#include <iomanip>
+
 using namespace std;
 
 
@@ -13,18 +15,13 @@ int main()
     double tau0 = 1, n = 1, n0, tau00;
     double K = 10, K0;
     double eps1 = 1e-9, eps2 = 1e-13;
-    double H = 0.001;
+    double H = 0.00001;
     int nit = 50000;
-    bool wasReached = false;
 
     for (int i = 0; i < nit; i++) {
         tau00 = tau0;
         K0 = K;
         n0 = n;
-
-        if (wasReached) {
-            break;
-        }
 
         F1 = 0, F2 = 0, F3 = 0;
         for (int i = 0; i < Nexp; i++) {
@@ -45,34 +42,23 @@ int main()
             n=n-H*F3;
 
             F = 0;
-            for (int i = 1; i < Nexp; ++i) {
+            for (int i = 0; i < Nexp; ++i) {
                 F+=pow(tau0+K*pow(x[i], n)-y[i], 2);
             }
         } while(F > F0);
 
         if ( pow(pow(tau0-tau00, 2)+pow(K-K0, 2)+pow(n-n0, 2), 0.5) < eps1) {
-            cout << "tauo: " << tau0 << endl;
-            cout << "K: " << K << endl;
-            cout << "n: " << n << endl;
-            cout << "F: " << F << endl;
-            cout << "F1: " << F1 << endl;
-            cout << "F2: " << F2 << endl;
-            cout << "F3: " << F3 << endl;
-            wasReached = true;
             break;
         }
 
         if (abs(F-F0)<eps2) {
-            cout << "tauo: " << tau0 << endl;
-            cout << "K: " << K << endl;
-            cout << "n: " << n << endl;
-            cout << "F: " << F << endl;
-            cout << "F1: " << F1 << endl;
-            cout << "F2: " << F2 << endl;
-            cout << "F3: " << F3 << endl;
-            wasReached = true;
             break;
         }
     }
+
+    cout <<  "tau0\t" << "K\t" << "n\t" << "F\t" << "F1\t" << "F2\t" << "F3\t" << endl;
+    cout.precision(4);
+    cout << fixed << tau0 << "\t" << K << "\t" << n << "\t" << F << "\t" << F1 << "\t" << F2 << "\t" << F3 << endl;
+
     return 0;
 }
